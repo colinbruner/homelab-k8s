b52151845464def066cffc07f81cde0be88c848f
Colin Bruner <code@colinbruner.com>
2025-03-24 19:00:30 -0500

    move out dns app resources and create root level for helm charts

--- k8s/bootstrap/base/infra/providers/http/generate.sh
+++ k8s/bootstrap/base/infra/providers/http/generate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# absolute path to the helm chart
+HELM_CHART=$(git rev-parse --show-toplevel)/k8s/packages/helm/cloudflare
 TEMP_DIR="temp_outputs"
 function generate_resources() {
   mkdir -p {resources/cloudflare/,$TEMP_DIR}
 
-  helm template -f values.yaml templates/cloudflare --output-dir=$TEMP_DIR >/dev/null
+  helm template -f values.yaml $HELM_CHART --output-dir=$TEMP_DIR >/dev/null
 
   rm -f resources/cloudflare/*.yaml
   mv $TEMP_DIR/cloudflare-http/templates/*.yaml resources/cloudflare/
